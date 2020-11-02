6ec8a9ca6f15913844c2cde2ff7352ad430bc2d0
William Huong <whuong@sfu.ca>
2020-11-01 16:30:59 -0800

    WIP testing code

--- codenames/codenames.js
+++ codenames/codenames.js
@@ -4,6 +4,11 @@
 */
 
 const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
 /*
 **	Define some values to be used
@@ -166,7 +171,7 @@ codenames.prototype.giveHint = function(hintWord, hintNum) {
 }
 
 /*
-**	Auxiliary Function
+**	Auxiliary Functions
 */
 
 // Read in the words list from a file. Expect one word per line
@@ -176,7 +181,7 @@ readWords = function() {
 
 	fs.readFile(word_list_path, 'ascii', (err, data) => {
 		if( err ) throw err;
-		//console.log(data.split(/[\r\n]+/));
+		// Regex to find newline on all operating systems
 		master_words_list = data.split(/[\r\n]+/);
 	})
 
@@ -219,9 +224,53 @@ function main() {
 	console.log("Object created");
 
 	console.log(game);
-	console.log(game.getGrid());
-	console.log(game.getMap());
-	console.log(game.getTurn());
+	
+	while( true ) {
+
+		console.log(game.getGrid());
+
+		var turnState = game.getTurn();
+
+		console.log("It is currently the", turnState.team + "'s turn right now");
+
+		if( turnState.phase == 'Hinting' ) {
+			console.log("Waiting for the ", turnState.team + "'s spymaster to give a hint:");
+
+			console.log(game.getMap());
+
+			var hintWord;
+			var hintNum;
+
+			rl.question("Please enter the hint word: ", (word) => {
+				hintWord = word;
+				rl.close();
+			});
+
+			rl.question("Please enter the hint number: ", (number) => {
+				hintNum = number;
+				rl.close();
+			})
+
+			setTimeout(game.giveHint(hintWord, hintNum), 5000);
+
+		} else {
+			console.log("Waiting for the ", turnState.team, "to make a guess");
+			console.log("The hint is:", game.hint.word, ":", game.hint.number);
+
+			console.log("Guess a word (Must be a perfect match of what the game has):");
+			
+			var guessWord;
+
+			rl.question("Please make a guess (The word must be a perfect match of what the game has): ", (guess) => {
+				guessWord = guess;
+				rl.close();
+			});
+
+			setTimeout(game.nameGuess(guessWord), 5000);
+
+		}
+
+	}
 
 }
 
