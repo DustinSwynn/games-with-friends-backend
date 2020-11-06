ac7abeeed3a09de791e5f6e2b8bb4f2c2226652f
dustins <dustin_seah@sfu.ca>
2020-11-05 23:13:51 -0800

    Started with Coup game. Not functional yet.

---
+++ src/coup/coup.js
@@ -0,0 +1,17 @@
+/*
+    Class for Coup game
+*/
+
+const { query } = require('express');
+const fs = require('fs');
+
+const influence = ['Duke','Assasin', 'Captain','Ambassador','Contessa'] // 3 of each influences in each game
+
+
+
+class coup {
+
+    constructor() {
+        
+    }
+}
